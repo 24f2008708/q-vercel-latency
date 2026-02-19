4ecdf1f03d668c0e0e04cd8f87f394173092fcaa
Harshit Abbi <24f2008708@ds.study.iitm.ac.in>
2026-02-19 17:30:36 +0530

    last fix

--- api/index.py
+++ api/index.py
@@ -11,9 +11,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
-    allow_credentials=False, 
+    expose_headers=["*"],
 )
 
 # Load JSON file
